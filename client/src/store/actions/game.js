cda75b23f6e7bb8fee4da4d1e5ace75dcf6727cd
Liza <lizakorneva1997@gmail.com>
2022-05-17 22:06:15 +0300

    store edits and game reducer creation

--- client/src/store/actions/game.js
+++ client/src/store/actions/game.js
@@ -1,6 +1,14 @@
 import { createAction } from '@reduxjs/toolkit';
 
+// Повысить уровень на 1 и добавить кубик на счетчик раундов
 export const upRound = createAction('game/round/up');
+// Удалить из общего запаса кубов те кубики, которые выпали в раунд
 export const useCubes = createAction('game/cubes/use');
+// Добавить кубы, которые выпали в игровую зону
 export const setDroppedСubes = createAction('game/dropped_cubes/set');
+// Заполнить игру общими целями
 export const setCommonGoals = createAction('game/common_goals/set');
+// Удалить оставшиеся выпавшие кубы из игры (после добавлления куба на счетчик раундов)
+export const deleteRemainingCubes = createAction('game/remaining_cubes/delete');
+// Заполнить игру инструментами
+export const setInstruments = createAction('game/instruments/set');
--- client/src/store/reducers/player.js
+++ client/src/store/reducers/player.js
@@ -25,28 +25,46 @@ const initialState = {
 };
 
 const player = createReducer(initialState, (builder) => {
+  // action принимает payload с ником игрока
   builder.addCase(actions.setLogin, (state, action) => {
     state.login = action.payload;
   });
+  // action принимает payload с объектом c ключами id и color
   builder.addCase(actions.setPersonalGoal, (state, action) => {
     state.personalGoal = action.payload;
   });
+  // action принимает payload с объектом c ключом color
   builder.addCase(actions.setWindowFrame, (state, action) => {
     state.windowFrame.color = action.payload;
   });
+  // action принимает payload с объектом витража c ключами:
+  // complexity (номер сложности) и pattern в виде двумерного массива
   builder.addCase(actions.setStainedGlass, (state, action) => {
     const { pattern, complexity } = action.payload;
     state.stainedGlass.complexity = complexity;
     state.stainedGlass.pattern = pattern;
   });
+  // action принимает payload с количеством заработанных очков
   builder.addCase(actions.setNumberPoints, (state, action) => {
     state.numberPoints = action.payload;
   });
+  // action принимает payload с числом фишек привелегий
   builder.addCase(actions.setPrivilegeСhips, (state, action) => {
     state.privilegeСhips = action.payload;
   });
+  // action принимает payload с числом фишек привелегий которые были использованы
   builder.addCase(actions.usePrivilegeСhips, (state, action) => {
     state.privilegeСhips -= action.payload;
+  });
+  // action принимает payload с массивом,
+  // где первый элемент - это номер вложенного массива шаблона
+  // второй - элемент вложенного массива,
+  // и третий - объект куба с ключами color и number
+  builder.addCase(actions.addCubeToStainedGlass, (state, action) => {
+    const cube = action.payload[3];
+    const x = action.payload[1];
+    const y = action.payload[0];
+    state.stainedGlass.pattern[y][x] = cube;
   });
 });
 
