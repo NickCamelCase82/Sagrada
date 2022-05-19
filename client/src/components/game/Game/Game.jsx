cd9ff6b5d79c1a76b958ef5e64402bd8f625c2d9
Merdzhen <merdzhen.k@gmail.com>
2022-05-19 13:15:12 +0300

    counter

--- client/src/components/game/Game/Game.jsx
+++ client/src/components/game/Game/Game.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import classes from './Game.module.css';
+import img from '../../../img/background.png';
+import Counter from '../Counter/Counter';
+// import Dice from '../Dice/Dice';
+
 
 const Game = () => {
-  return <div>Game</div>;
+  return (
+    <div className={classes.gamePage}>
+      <div className={classes.gameImgDiv}>
+        <img src={img} alt="sagrada-img" className={classes.gameImg} />
+        <Counter />
+      </div>
+    </div>
+  );
 };
 
 export default Game;
