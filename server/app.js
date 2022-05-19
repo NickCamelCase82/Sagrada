eb08349d77f10d7413b3b3efe516a5ce2bd9741a
Nick <nick.egorenkov@gmail.com>
2022-05-19 00:15:02 -0700

    Registration, Login, Logout from server side

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
-const path = require('path');
-const cors = require('cors');
-require('dotenv').config();
-const dbCheck = require('./db/dbConnectionCheck');
+const path = require("path");
+const cors = require("cors");
+require("dotenv").config();
+const dbCheck = require("./db/dbConnectionCheck");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+
+const userRouter = require("./routes/userRouter");
 
 const app = express();
 
@@ -12,18 +16,34 @@ const PORT = process.env.PORT ?? 3002;
 
 const corsConfig = {
   // Домены которым разрешен доступ к файлам
-  origin: ['http://localhost:3000'],
+  origin: ["http://localhost:3000"],
 };
 
 app.use(cors(corsConfig));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", userRouter);
 
 dbCheck();
 
+const sessionConfig = {
+  store: new FileStore(),
+  name: "MyCookie",
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    httpOnly: true,
+  },
+};
+
+app.use(session(sessionConfig));
+
 app.listen(PORT, () => {
-  console.log(`server started on PORT: ${PORT}`);
+  console.log(`Server is up and running on PORT: ${PORT}`);
 });
