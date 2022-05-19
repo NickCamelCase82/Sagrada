56a2d2beda0228560dce3c289006bd8eaed643b0
Liza <lizakorneva1997@gmail.com>
2022-05-19 14:18:53 +0300

    created by prettierrc and cleaned up code

--- client/src/components/game/CommonGoalCards/CommonGoalCards.jsx
+++ client/src/components/game/CommonGoalCards/CommonGoalCards.jsx
@@ -2,17 +2,12 @@ import React from 'react';
 import './CommonGoalCards.css';
 import { CommonGoals } from '../../../constans/constans';
 import { randomCommonGoals } from '../../../features/gameFeatures';
-
-import colorDiagonals from '../../../img/common-goals/ColorDiagonals.jpg';
-import colorVariety from '../../../img/common-goals/ColorVariety.jpg';
-import columnColorVariety from '../../../img/common-goals/ColumnColorVariety.jpg';
 import Сarousel from '../../Сarousel/Сarousel';
 
 const CommonGoalCards = () => {
-  console.log(randomCommonGoals(3, CommonGoals));
   return (
     // <div className="container-common-goals">
-    //   {randomCommonGoals().map((card) => {
+    //   {randomCommonGoals(3, CommonGoals).map((card) => {
     //     return <CommonGoal card={card} key={card.id} />;
     //   })}
     // </div>
