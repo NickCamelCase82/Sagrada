107dc54fa27d620b7d22ef0b5bc37528e9118729
Liza <lizakorneva1997@gmail.com>
2022-05-21 23:54:37 +0300

    implemented the logic of raising a cube from the reserve of cubes

--- client/src/store/actions/game.js
+++ client/src/store/actions/game.js
@@ -8,6 +8,8 @@ export const removeCubes = createAction('game/cubes/remove');
 export const setActivePlayer = createAction('game/active_player/set');
 // Добавить кубы, которые выпали в игровую зону
 export const setDroppedСubes = createAction('game/dropped_cubes/set');
+// Удалить из резерва тот кубик, который взяли
+export const removeDroppedСube = createAction('game/dropped_cube/remove');
 // Заполнить игру общими целями
 export const setCommonGoals = createAction('game/common_goals/set');
 // Удалить оставшиеся выпавшие кубы из игры (после добавлления куба на счетчик раундов)
